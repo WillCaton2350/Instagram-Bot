88c3b673aa8f9cfc2967b9dab8514ace0207c49b
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2023-08-05 21:16:03 +0000

    Update dataset.py

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -1,9 +1,4 @@
-firstNameArray = ["David", "John", "Jackson", "Molly", "Mariah", "Summer", "Savvanah", 
-                  "Carolina", "Brianna", "Shelby", "Augustus", "Quinn", "Noah", "Bailey", 
-                  "Jess", "Armani", "Maggie", "Vince", "Hannah","Jared","Misty","Cameron"]
-lastNameArray = ["Walker", "Peters", "Johnson", "Berg", "Milton", "James", "Butler", 
-                 "Kalle", "Dale", "Washington", "Smith", "Clarkson", "Jones", "Murray", 
-                 "Mitchell", "Green", "Thompson", "Lyon", "Gail", "Bennett", "Carter"]
+
 symbolsArray =[".","1","2","3","4","5","6","7","8",
                "9","10","11","12","13","14","15","16",
                "17","18","19","20","21","22","23","24"]
