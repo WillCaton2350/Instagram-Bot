694214875f63c6666dfb5f36cf5e492bc4db1552
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2023-08-05 21:23:50 +0000

    Update Bot.py

--- Brute/Bot.py
+++ Brute/Bot.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait as WDW
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from urllib.error import HTTPError
-from Dataset.dataset import firstNameArray,symbolsArray,lastNameArray,alphaNumericArray
+from Dataset.dataset import symbolsArray,alphaNumericArray
 import random
 import time
 
@@ -25,24 +25,10 @@ class WebDriver:
             print(err,"Page Not Found")
         time.sleep(2)
     def login(self):
-        self.driver.find_element(By.ID, "createacc").click()
-        time.sleep(3)
-        WDW(self.driver, timeout=10).until(EC.visibility_of_element_located((By.ID, "usernamereg-firstName")))
-        firstName_element = self.driver.find_element(By.ID, "usernamereg-firstName")
-        firstName = random.choice(firstNameArray)
-        firstName_element.send_keys(firstName)
-
-        lastName_element = self.driver.find_element(By.ID, "usernamereg-lastName")
-        lastName = random.choice(lastNameArray)
-        lastName_element.send_keys(lastName)
-
-        username = firstName + random.choice(symbolsArray) + lastName + str(random.randint(10, 500))
-        self.driver.find_element(By.ID, "usernamereg-userId").send_keys(username)
 
         password = ''.join([random.choice(symbolsArray) for _ in range(3)]) + str(random.choice(alphaNumericArray)) + str(random.randint(10, 500))
         self.driver.find_element(By.ID, "usernamereg-password").send_keys(password) 
 
-        self.driver.find_element(By.NAME,"username").send_keys(username)
         self.driver.find_element(By.NAME,"password").send_keys(password)
 
 
